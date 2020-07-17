421266f874a3dfb079f5d9e1ef8a67ce771fba20
jburke729 <junaid_burke@hotmail.com>
2020-07-17 11:26:02 -0400

    style the search movie component

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -6,7 +6,7 @@ export default function SearchMovies() {
       <label htmlFor="query" className="label">
         Movie Name
       </label>
-      <input type="text" name="query" placeholder="i.e. Jurrasic Parck" />
+      <input type="text" name="query" className="input" placeholder="i.e. Jurasic Park" />
       <button className="button" type="submit">
         Search
       </button>
