b6356b970db3cdf27898d10479926116ed9bbaee
jburke729 <junaid_burke@hotmail.com>
2020-07-17 08:50:10 -0400

    added styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './style.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <h1 className="title"> React Search Movie</h1>
+    </div>
+  );
 }
 
 export default App;
